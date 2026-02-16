74cbdd1d8b9b7796f5f7404b160f3a18fcf2b214
Madhupa Chamikara <chamikara1992@outlook.com>
2026-02-16 14:55:54 +1300

    Update the example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -36,7 +36,12 @@ test('check java page', async ({ page }) => {
   
   await page.getByRole('link', { name: 'Get started' }).click();
   await page.getByRole('button', { name: 'Node.js' }).hover();
-  await page.getByText('Java', { exact: true }).click();
+  await page.getByRole('link', { name: /^Java$/}).click();
+  await expect(page).toHaveURL('https://playwright.dev/java/docs/intro');
+  //await expect(page.getByText('Installing Playwright')).not.toBeVisible();
+  await expect(page.getByText('Installing Playwright', { exact: true })).not.toBeVisible();
+  //await expect(page.getByText('Installing Playwright')).toBeHidden();
+  await expect(page.getByText('Playwright is distributed as a set of Maven modules. The easiest way to use it is to add one dependency to your project\'s pom.xml as described below. If you\'re not familiar with Maven please refer to its documentation.')).toBeVisible();
   
 });
 
